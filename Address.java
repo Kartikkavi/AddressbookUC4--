e67545ebbbf5a3f0a1429a05fffb5983a8463cab
kartik <kavimandankartik33@gmail.com>
2021-04-06 01:41:48 +0700

    Added:delete the person data

--- Address.java
+++ Address.java
@@ -12,7 +12,7 @@ public class Address {
    Long zipCode, phoneNum;
     
     // to return data to object by user inputs  
-    public void setContactsDetails(){      
+    public void setContactsDetails() {      
         System.out.println("Enter your First name: ");  
         this.fName = userInputs.nextLine();
         System.out.println("Enter your Last name: ");
@@ -31,9 +31,9 @@ public class Address {
       this.email = userInputs.nextLine();
     }
 
-  
+    //to read data from contacts
     public String showContactsDetails() {
-        return ("------------------------First Name: " + fName + "\nLast Name: " + lName + "\nAddress: " + address + "\nCity: " + city + "\nState: " + state + "\nPin Code: " + zipCode + "\nPhone Number: " + phoneNum + "\nEmail ID: " + email + "\n-------------------------");
+        return ("------------------------\nFirst Name: " + fName + "\nLast Name: " + lName + "\nAddress: " + address + "\nCity: " + city + "\nState: " + state + "\nPin Code: " + zipCode + "\nPhone Number: " + phoneNum + "\nEmail ID: " + email + "\n-------------------------");
     }
     
     // get complete details of person
@@ -53,10 +53,10 @@ public class Address {
         List<Address> persons = new ArrayList<>();
 
         Scanner uc = new Scanner(System.in);
-        System.out.println("If you want to add address enter '0' ");
+        System.out.println("If you want to add address enter '0' or \nfor editing existing address enter '1' and \nfor deleting enter '2': ");
         int userChoice = uc.nextInt();
 
-        if(userChoice == 0 ){
+        if(userChoice == 0 ) {
             //adding contact details to address book
             System.out.println("Adding a new Address");
 
@@ -92,23 +92,23 @@ public class Address {
                     if(i.getFirstName().equals(editContact)) {
                 
                         System.out.println("Change the First Name: ");
-                        String editfName = input.next();
+                        String editfName = input.nextLine();
                         i.fName = editfName;
                     
                         System.out.println("Change the Last Name: ");
-                        String editlName = input.next();
+                        String editlName = input.nextLine();
                         i.lName = editlName;
 
                         System.out.println("Change the Address: ");
-                        String editAddress = input.next();
+                        String editAddress = input.nextLine();
                         i.address = editAddress;
 
                         System.out.println("Change the City: ");
-                        String editCity = input.next();
+                        String editCity = input.nextLine();
                         i.city = editCity;
 
                         System.out.println("Change the State: ");
-                        String editState = input.next();
+                        String editState = input.nextLine();
                         i.state = editState;
 
                         System.out.println("Change the Pin Code: ");
@@ -120,17 +120,33 @@ public class Address {
                         i.phoneNum = editPhoneNum;
         
                         System.out.println("Change the Email ID: ");
-                        String editEmail = input.next();
+                        String editEmail = input.nextLine();
                         i.email = editEmail;
                     }
                 System.out.println(persons);
-            
+                }      
+              
             }
+            
+                System.out.println("Deleting a contact");
+
+                System.out.println(persons);
+                System.out.println("Which contact you want to delete from address book: ");
+                Scanner input = new Scanner(System.in);
+                String deleteContact = input.next();
+
+                for(Address i : persons) {
+
+                        if(i.getFirstName().equals(deleteContact)){
+                            persons.remove(i);
+                        }
+                }
+                System.out.println(persons);
+                
             }else{
                 System.out.println("Invalid Option");
             }
         }
-    }
-
-}
+    
+	}
 
